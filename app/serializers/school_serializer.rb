d9b5cdce2649d594ff1b5511f7efac1c50ae91b5
jpetrucci <jpetrucci49@yahoo.com>
2018-09-01 21:06:11 -0400

    Add auth before actions
    Add :editable to school serializer

--- app/serializers/school_serializer.rb
+++ app/serializers/school_serializer.rb
@@ -1,6 +1,6 @@
 class SchoolSerializer < ActiveModel::Serializer
   has_one :user
-  attributes :id, :name, :address, :city, :phone, :zip, :user_id, :editable
+  attributes :id, :name, :address, :city, :phone, :zip, :user, :editable
   def editable
     scope == object.user
   end
